d9689682c2026e64798f3360a1b744636a5f2c70
DiazAle5 <youarereta@gmail.com>
2025-05-15 19:26:52 -0400

    Made fadein/fadeout
    Combined w/ teleporting the player back to the bed, this will give us a new day

---
+++ Assets/_Scripts/ScreenFade.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFade : MonoBehaviour
+{
+    public CanvasGroup canvasgroup;
+    float currentAlpha;
+    float desiredAlpha;
+    public float fadeSpeed;
+    public bool startFading;
+
+    void Start()
+    {
+        currentAlpha = 0;
+    }
+
+    private void Update()
+    {
+        currentAlpha = Mathf.MoveTowards(currentAlpha, desiredAlpha, fadeSpeed * Time.deltaTime);
+
+        canvasgroup.alpha = currentAlpha;
+
+        //This is just to test the script in the Unity editor, from script we can just call Fade() instead of making startFading true
+        if (startFading)
+        {
+            Fade();
+        }
+        Debug.Log(currentAlpha);
+        Debug.Log(desiredAlpha);
+    }
+
+    void Fade()
+    {
+        startFading = false;
+        if(currentAlpha == 0)
+        {
+            desiredAlpha = 1;
+        }
+        else
+        {
+            desiredAlpha = 0;
+        }
+    }
+}
