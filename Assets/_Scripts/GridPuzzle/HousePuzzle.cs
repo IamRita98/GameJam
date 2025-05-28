8e9f17923cd8524461cca6f86194a2f13857c9be
Lashvir <dimagax01@gmail.com>
2025-05-28 00:27:30 -0400

    added more to house puzzle scripts

---
+++ Assets/_Scripts/GridPuzzle/HousePuzzle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class HousePuzzle : MonoBehaviour
+{
+
+    public Transform[] pieces;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setting up board
+        int x = 0;
+        int y = 0;
+        foreach (Transform child in PuzzleGridBase.transform)
+        {
+            PuzzlePIece piece = child.GetComponent<PuzzlePiece>();
+            if (piece != null)
+            {
+                piece.onClicked.AddListener(OnPieceClicked);
+            }
+            if (y < 3)
+            {
+                pieces[x, y++] = child;
+                continue;
+            }
+            x++;
+        }
+        x = 0;
+        y = 0;
+        Transform temp;
+    }
+    private void OnPieceClicked(GameObject piece)
+    {
+
+    }
+}
+
